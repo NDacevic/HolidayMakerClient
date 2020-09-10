b052006f65fb7bd17ce1c4d4c771837a3a92bb32
NDacevic <n.dacevic@gmail.com>
2020-09-10 13:43:00 +0200

    Created user class

---
+++ HolidayMakerClient/HolidayMakerClient/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidayMakerClient
+{
+    class User
+    {
+        #region Constant Fields
+        #endregion
+
+        #region Fields
+        
+        #endregion
+
+        #region Constructors
+        public User()
+        {
+        }
+        #endregion
+
+        #region Delegates
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Properties
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsCompany { get; set; }
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
