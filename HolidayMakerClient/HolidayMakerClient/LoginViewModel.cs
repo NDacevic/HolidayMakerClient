ba62f57f58ca9b57e18ac1da8b2e6bde161e8741
NDacevic <n.dacevic@gmail.com>
2020-09-10 14:30:26 +0200

    Created LoginViewModel

--- HolidayMakerClient/HolidayMakerClient/LoginViewModel.cs
+++ HolidayMakerClient/HolidayMakerClient/LoginViewModel.cs
@@ -6,7 +6,47 @@ using System.Threading.Tasks;
 
 namespace HolidayMakerClient
 {
-    class LoginViewModel
+    public class LoginViewModel
     {
+        #region Constant Fields
+        #endregion
+
+        #region Fields
+        #endregion
+
+        #region Constructors
+        public LoginViewModel()
+        {
+
+        }
+
+        public LoginViewModel(User activeUser)
+        {
+            ActiveUser = activeUser;
+        }
+        #endregion
+
+        #region Delegates
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Properties
+        public User ActiveUser { get; set; }
+
+        #endregion
+
+        #region Methods
+        public void GetUser()
+        {
+
+        }
+
+        public void ConfirmPassword()
+        {
+
+        }
+        #endregion
     }
 }
