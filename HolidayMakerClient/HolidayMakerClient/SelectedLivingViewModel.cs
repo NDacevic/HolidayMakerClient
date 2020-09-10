f5cfabd8c6f9da0a856dc9159774e28154751f02
NDacevic <n.dacevic@gmail.com>
2020-09-10 14:25:29 +0200

    Created SelectedLivingViewModel

---
+++ HolidayMakerClient/HolidayMakerClient/SelectedLivingViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidayMakerClient
+{
+    public class SelectedLivingViewModel
+    {
+        #region Constant Fields
+        #endregion
+
+        #region Fields
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Delegates
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Properties
+        public Home SelectedHome { get; set; }
+        public Reservation SelectedReservation { get; set; }
+        #endregion
+
+        #region Methods
+        public void CreateReservation()
+        {
+
+        }
+
+        public void DeleteReservation()
+        {
+
+        }
+
+        public void EditReservation()
+        {
+
+        }
+        #endregion
+    }
+}
