8a504cd76124a850e0e239e13cb0fb46cd9c0fc1
NDacevic <n.dacevic@gmail.com>
2020-09-10 13:51:27 +0200

    Created Reservation Class

---
+++ HolidayMakerClient/HolidayMakerClient/Reservation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace HolidayMakerClient
+{
+    class Reservation
+    {
+        #region Constant Fields
+        #endregion
+
+        #region Fields
+        #endregion
+
+        #region Constructors
+        public Reservation()
+        {
+
+        }
+        #endregion
+
+        #region Delegates
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Properties
+        public int ReserationId { get; set; }
+        public int HomeId { get; set; }
+        public int UserId { get; set; }
+        public DateTimeOffset StartDate { get; set; }
+        public DateTimeOffset EndDate { get; set; }
+        public decimal TotalPrice { get; set; }
+        public List<Addon> AddonList { get; set; }
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
