c2d67d673fcebb0f84a7367569423deed1ee79ec
juliaalexandra <56877873+juliaalexandra@users.noreply.github.com>
2020-09-14 14:50:20 +0200

    HttpResponsMessage tillagt

--- HolidayMakerClient/HolidayMakerClient/ApiHelper.cs
+++ HolidayMakerClient/HolidayMakerClient/ApiHelper.cs
@@ -85,22 +85,29 @@ namespace HolidayMakerClient
         {
             List<Addon> addonList = new List<Addon>();
             try
-            {
-                jsonString = await httpClient.GetStringAsync("Addons");
-                var addon = JsonConvert.DeserializeObject<List<Addon>>(jsonString);
-                foreach(var a in addon)
+            {               
+                HttpResponseMessage response = await httpClient.GetAsync("Addons");
+                if(response.IsSuccessStatusCode)
                 {
-                    addonList.Add(a);
+                     jsonString = response.Content.ReadAsStringAsync().Result;
+                     var addon = JsonConvert.DeserializeObject<List<Addon>>(jsonString);
+                     foreach(var a in addon)
+                     {
+                        addonList.Add(a);
+                     }
+                        return addonList;
                 }
-                return addonList;
+                else
+                {
+                    throw new HttpRequestException("Gick ej att hämta, vänligen försök igen.");
+                }
             }
             catch (Exception exc)
             {
                 BasicNoConnectionMessage(exc);
                 return addonList;
             }
-
-    }
+        }
         private static async void BasicNoConnectionMessage(Exception exc)
         {
             Debug.WriteLine(exc.Message);
