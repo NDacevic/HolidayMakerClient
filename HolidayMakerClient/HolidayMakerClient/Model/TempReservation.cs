bef1d52de553f5aebc0e38ef8ac613717110f33a
juliaalexandra <56877873+juliaalexandra@users.noreply.github.com>
2020-09-15 10:13:19 +0200

    Skapade TempReservation

---
+++ HolidayMakerClient/HolidayMakerClient/Model/TempReservation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidayMakerClient.Model
+{
+    public class TempReservation
+    {
+        #region Constant Fields
+        #endregion
+
+        #region Fields
+        #endregion
+
+        #region Constructors
+        public TempReservation(int numberOfGuests, DateTime startDate, DateTime endDate, Home home)
+        {
+            NumberOfGuests = numberOfGuests;
+            StartDate = startDate;
+            EndDate = endDate;
+            Home = home;
+        }
+        #endregion
+
+        #region Delegates
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Properties
+        public int NumberOfGuests { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Home Home { get; set; }
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
