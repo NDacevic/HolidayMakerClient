ccd2ccfedbf12dada728dded55498dd2d74bcad2
JohnnyK <56879977+JohnnyKrook@users.noreply.github.com>
2020-09-15 14:49:47 +0200

    Added try catch in Apihelper

--- HolidayMakerClient/HolidayMakerClient/Helpers/ApiHelper.cs
+++ HolidayMakerClient/HolidayMakerClient/Helpers/ApiHelper.cs
@@ -122,42 +122,59 @@ namespace HolidayMakerClient
         }
         public async Task<ObservableCollection<Reservation>> GetUserReservations()
         {
-            //TODO:Send in active users id instead of "1"
-            HttpResponseMessage response = await httpClient.GetAsync("UsersReservations/1");
+            try
+            {
+                //TODO:Send in active users id instead of "1"
+                HttpResponseMessage response = await httpClient.GetAsync("UsersReservations/1");
 
-            if (response.IsSuccessStatusCode)
-            {
-                jsonString = response.Content.ReadAsStringAsync().Result;
-               
-                var reservations = JsonConvert.DeserializeObject<ObservableCollection<Reservation>>(jsonString);
+                if (response.IsSuccessStatusCode)
+                {
+                    jsonString = response.Content.ReadAsStringAsync().Result;
 
-                return reservations;
+                    var reservations = JsonConvert.DeserializeObject<ObservableCollection<Reservation>>(jsonString);
+
+                    return reservations;
+                }
+                else if (response.Content == null)
+                {
+                    throw new HttpRequestException("Här var det tomt! Gå in och gör en reservation för att se listan.");
+                }
+                else
+                {
+                    throw new HttpRequestException("Kunde inte hämta några reservationer, var vänlig försök igen.");
+                }
             }
-            else if(response.Content==null)
+            catch (Exception exc)
             {
-                throw new HttpRequestException("Här var det tomt! Gå in och gör en reservation för att se listan.");
+                BasicNoConnectionMessage(exc);
+                return new ObservableCollection<Reservation>();
             }
-            else
-            {
-                throw new HttpRequestException("Kunde inte hämta några reservationer, var vänlig försök igen.");
-            }
+
         }
         public async Task <Home> GetHome(int id)
         {
-            //Used to get Home details from the selected reservation in MyPage
-            HttpResponseMessage response = await httpClient.GetAsync($"Homes/{id}");
-            if (response.IsSuccessStatusCode)
+           try
             {
-                jsonString = response.Content.ReadAsStringAsync().Result;
+                HttpResponseMessage response = await httpClient.GetAsync($"Homes/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    jsonString = response.Content.ReadAsStringAsync().Result;
 
-                var home = JsonConvert.DeserializeObject<Home>(jsonString);
+                    var home = JsonConvert.DeserializeObject<Home>(jsonString);
 
-                return home;
+                    return home;
+                }
+                else
+                {
+                    throw new HttpRequestException("Kunde inte hämta några boende, var vänlig försök igen.");
+                }
             }
-            else
+            catch (Exception exc)
             {
-                throw new HttpRequestException("Kunde inte hämta några boende, var vänlig försök igen.");
+                BasicNoConnectionMessage(exc);
+                return new Home();
             }
+
         }
 
         public static void GetReservation()
@@ -181,20 +198,29 @@ namespace HolidayMakerClient
         }
         public async Task<ObservableCollection<Addon>> GetReservationAddon(int id)
         {
-            //TODO: Send in reservation id, get a list of addons linked to that reservation and return.
-            HttpResponseMessage response = await httpClient.GetAsync($"ReservationAddons/{id}");
-            if (response.IsSuccessStatusCode)
+            try
             {
-                jsonString = response.Content.ReadAsStringAsync().Result;
+                //TODO: Send in reservation id, get a list of addons linked to that reservation and return.
+                HttpResponseMessage response = await httpClient.GetAsync($"ReservationAddons/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    jsonString = response.Content.ReadAsStringAsync().Result;
 
-                var addons = JsonConvert.DeserializeObject<ObservableCollection<Addon>>(jsonString);
+                    var addons = JsonConvert.DeserializeObject<ObservableCollection<Addon>>(jsonString);
 
-                return addons;
+                    return addons;
+                }
+                else
+                {
+                    throw new HttpRequestException("Kunde inte hämta några boende, var vänlig försök igen.");
+                }
             }
-            else
+            catch (Exception exc)
             {
-                throw new HttpRequestException("Kunde inte hämta några boende, var vänlig försök igen.");
+                BasicNoConnectionMessage(exc);
+                return new ObservableCollection<Addon>();
             }
+
         }
         public async Task<List<Addon>> GetAllAddon ()
         {
