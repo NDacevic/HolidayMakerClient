82005c6dd9cdb946bd9ac2da68b8a37b0e26d09e
mikaelollhage <mikaelollhage@gmail.com>
2020-09-21 14:51:54 +0200

    UploadLivingView and UploadLivingViewModel added

---
+++ HolidayMakerClient/HolidayMakerClient/ViewModel/UploadLivingViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidayMakerClient.ViewModel
+{
+    public class UploadLivingViewModel
+    {
+        #region Constant Fields
+        #endregion
+
+        #region Fields
+        #endregion
+
+        #region Constructors
+        public UploadLivingViewModel()
+        {
+        }
+
+        #endregion
+
+        #region Delegates
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Methods
+        #endregion
+
+    }
+}
