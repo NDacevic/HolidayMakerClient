5014ccf1abcde5451f6c86ac0b53e106c3055096
NDacevic <n.dacevic@gmail.com>
2020-09-21 13:21:36 +0200

    Added 2 value converters that are to be used on the SearchPage

---
+++ HolidayMakerClient/HolidayMakerClient/Converters/BooleanSwedishConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace HolidayMakerClient.Converters
+{
+    public class BooleanSwedishConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (((bool)value) == true)
+                return "Ja";
+            else
+                return "Nej";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
