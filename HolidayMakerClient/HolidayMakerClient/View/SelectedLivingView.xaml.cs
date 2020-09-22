bbaf3b665b089918f80259965ba0eafaccb8230c
NDacevic <n.dacevic@gmail.com>
2020-09-22 13:51:38 +0200

    Found some small bugs and a merge conflict that needed fixing for the build process to finish

--- HolidayMakerClient/HolidayMakerClient/View/SelectedLivingView.xaml.cs
+++ HolidayMakerClient/HolidayMakerClient/View/SelectedLivingView.xaml.cs
@@ -166,9 +166,10 @@ namespace HolidayMakerClient
             try
             {
                 Addon ad = ((Addon)lv_DisplayAddons.SelectedItem);
-                if (ad.AddonType == "Extrasäng") cb_ExtraBed.IsChecked = false;
+                if (ad.AddonType == "Extrasäng") 
+                    Cb_ExtraBed.IsChecked = false;
                 else if(ad.AddonType == "All-inclusive" || ad.AddonType == "Helpension" || ad.AddonType == "Halvpension") Rb_noPension.IsChecked = true;
-                ChosenAddons.Remove((Addon)lv_DisplayAddons.SelectedItem);
+                    ChosenAddons.Remove((Addon)lv_DisplayAddons.SelectedItem);
                 
             }
             catch (Exception)
@@ -348,7 +349,6 @@ namespace HolidayMakerClient
         {
             Frame.Navigate(typeof(SearchView));
         }
-<<<<<<< HEAD
         /// <summary>
         /// Navigate to MyPage 
         /// </summary>
@@ -358,9 +358,5 @@ namespace HolidayMakerClient
         {
             Frame.Navigate(typeof(MyPageView));
         }
-=======
-
-
->>>>>>> dev
     }
 }
